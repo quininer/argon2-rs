f7e8887b1cf5920410e7edee190a3e69217eae92
quininer kel <quininer@live.com>
2016-03-15 22:50:53 +0800

    update(dependencies) ffigen

--- README.md
+++ README.md
@@ -12,18 +12,3 @@ Requirements
 	* Arch user can install from [AUR](https://aur.archlinux.org/packages/argon2-git/).
 + Clang
 	* Generate `ffi.rs`.
-
-Build
------
-
-You can use environment variables to specify include path.
-
-fish:
-
-	set -x INCLUDE_PATH /usr/local/include/
-	set -x CLANG_INCLUDE_PATH /usr/lib/clang/3.4.0/include/
-
-bash:
-
-	export INCLUDE_PATH=/usr/local/include/
-	export CLANG_INCLUDE_PATH=/usr/lib/clang/3.4.0/include/
--- build.rs
+++ build.rs
@@ -1,21 +1,5 @@
-extern crate ffigen;
-
-use std::env::var;
-use std::fs::File;
-use std::io::Write;
-use ffigen::GenOptions;
-
-const CLANG_INCLUDE_PATH: &'static str = "/usr/lib/clang/3.7.1/include/";
-const INCLUDE_PATH: &'static str = "/usr/include/";
-
+#[macro_use] extern crate ffigen;
 
 fn main() {
-    let data = GenOptions::default()
-        .arg(&format!("-I{}", var("CLANG_INCLUDE_PATH").unwrap_or(CLANG_INCLUDE_PATH.into())))
-        .header(&format!("{}{}", var("INCLUDE_PATH").unwrap_or(INCLUDE_PATH.into()), "argon2.h"))
-        .link("argon2")
-        .gen();
-
-    File::create("src/ffi.rs").unwrap()
-        .write(&data).unwrap();
+    gen!("argon2", [ "argon2.h" ] -> "src/ffi.rs");
 }
